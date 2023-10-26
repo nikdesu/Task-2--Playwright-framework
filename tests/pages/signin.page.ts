9b0bc805beda8a3f7560bbd28d1476f45af2b621
nikdesu <ostspchyk1409@gmail.com>
2023-10-26 18:40:07 +0300

    Formated code

--- tests/pages/signin.page.ts
+++ tests/pages/signin.page.ts
@@ -1,31 +1,29 @@
-import { expect, Locator, Page  } from "@playwright/test"
-import credentials from "../../credentials.json"
-import { faker } from '@faker-js/faker'
+import { expect, Locator, Page } from "@playwright/test";
+import credentials from "../../credentials.json";
+import { faker } from "@faker-js/faker";
 
-export class SignInPage 
-{ 
-  readonly page: Page
-  readonly loginTextbox: Locator
-  readonly passTextbox: Locator
-  readonly logInBtn: Locator
-  readonly invalidCredsErr: Locator
+export class SignInPage {
+  readonly page: Page;
+  readonly loginTextbox: Locator;
+  readonly passTextbox: Locator;
+  readonly logInBtn: Locator;
+  readonly invalidCredsErr: Locator;
 
-  constructor(page: Page) { 
-    this.page = page
-    this.loginTextbox = page.locator("#username")
-    this.passTextbox = page.locator("#password")
-    this.logInBtn = page.locator("#login-submit")
-    this.invalidCredsErr = page.locator("#flash_error")
+  constructor(page: Page) {
+    this.page = page;
+    this.loginTextbox = page.locator("#username");
+    this.passTextbox = page.locator("#password");
+    this.logInBtn = page.locator("#login-submit");
+    this.invalidCredsErr = page.locator("#flash_error");
   }
 
-  //Actions
   async enterCredDet() {
-    await this.loginTextbox.fill(credentials.username)
-    await this.passTextbox.fill(credentials.password)
+    await this.loginTextbox.fill(credentials.username);
+    await this.passTextbox.fill(credentials.password);
   }
 
   async enterINVCredDet() {
-    await this.loginTextbox.fill(faker.internet.email())
-    await this.passTextbox.fill(faker.internet.password())
+    await this.loginTextbox.fill(faker.internet.email());
+    await this.passTextbox.fill(faker.internet.password());
   }
 }
--- tests/tests1.spec.ts
+++ tests/tests1.spec.ts
@@ -1,75 +1,75 @@
-import { test, expect } from '@playwright/test'
-import { MainPage }   from './pages/main.page'
-import { SignInPage } from './pages/signin.page'
+import { test, expect } from "@playwright/test";
+import { MainPage } from "./pages/main.page";
+import { SignInPage } from "./pages/signin.page";
 
-test('Sign in with valid creds', async ({ page }) => {
-  const mainpage = new MainPage (page)
-  const signinpage = new SignInPage (page) 
+test("Sign in with valid creds", async ({ page }) => {
+  const mainpage = new MainPage(page);
+  const signinpage = new SignInPage(page);
 
-  await mainpage.goto()
-  await mainpage.signInBtn.click()
-  await signinpage.enterCredDet()
-  await signinpage.logInBtn.click()
-  await expect(mainpage.signOutBtn).toBeVisible() 
+  await mainpage.goto();
+  await mainpage.signInBtn.click();
+  await signinpage.enterCredDet();
+  await signinpage.logInBtn.click();
+  await expect(mainpage.signOutBtn).toBeVisible();
 });
 
-test('Sign in with invalid creds', async ({ page }) => {
-  const mainpage = new MainPage (page)
-  const signinpage = new SignInPage (page) 
+test("Sign in with invalid creds", async ({ page }) => {
+  const mainpage = new MainPage(page);
+  const signinpage = new SignInPage(page);
 
-  await mainpage.goto()
-  await mainpage.signInBtn.click()
-  await signinpage.enterINVCredDet()
-  await signinpage.logInBtn.click()
-  await expect(signinpage.invalidCredsErr).toBeVisible()
-})
+  await mainpage.goto();
+  await mainpage.signInBtn.click();
+  await signinpage.enterINVCredDet();
+  await signinpage.logInBtn.click();
+  await expect(signinpage.invalidCredsErr).toBeVisible();
+});
 
-test('Sign out feature', async ({ page }) => {
-  const mainpage = new MainPage (page)
-  const signinpage = new SignInPage (page) 
+test("Sign out feature", async ({ page }) => {
+  const mainpage = new MainPage(page);
+  const signinpage = new SignInPage(page);
 
-  await mainpage.goto()
-  await mainpage.signInBtn.click()
-  await signinpage.enterCredDet()
-  await signinpage.logInBtn.click()
-  await mainpage.signOutBtn.click()
-  await expect(mainpage.signInBtn).toBeVisible()
-})
+  await mainpage.goto();
+  await mainpage.signInBtn.click();
+  await signinpage.enterCredDet();
+  await signinpage.logInBtn.click();
+  await mainpage.signOutBtn.click();
+  await expect(mainpage.signInBtn).toBeVisible();
+});
 
-test('Main menu functionality', async ({page}) => {
-  const mainpage = new MainPage (page)
+test("Main menu functionality", async ({ page }) => {
+  const mainpage = new MainPage(page);
 
-  await mainpage.goto()
-  await mainpage.OverviewBtn.click()
-  await mainpage.closeAdIfVisible()
-  await expect(mainpage.page).toHaveURL(/redmine/)
-  await mainpage.DownloadBtn.click()
-  await mainpage.closeAdIfVisible()
-  await expect(mainpage.page).toHaveURL(/Download/)
-  await mainpage.ActivityBtn.click()
-  await expect(mainpage.page).toHaveURL(/activity/)
-  await mainpage.RoadmapBtn.click()
-  await expect(mainpage.page).toHaveURL(/roadmap/)
-  await mainpage.IssuesBtn.click()
-  await expect(mainpage.page).toHaveURL(/issues/)
-  await mainpage.NewsBtn.click()
-  await expect(mainpage.page).toHaveURL(/news/)
-  await mainpage.WikiBtn.click()
-  await expect(mainpage.page).toHaveURL(/wiki/)
-  await mainpage.ForumsBtn.click()
-  await expect(mainpage.page).toHaveURL(/boards/)
-  await mainpage.RepositoryBtn.click()
-  await expect(mainpage.page).toHaveURL(/repository/)
-})
+  await mainpage.goto();
+  await mainpage.OverviewBtn.click();
+  await mainpage.closeAdIfVisible();
+  await expect(mainpage.page).toHaveURL(/redmine/);
+  await mainpage.DownloadBtn.click();
+  await mainpage.closeAdIfVisible();
+  await expect(mainpage.page).toHaveURL(/Download/);
+  await mainpage.ActivityBtn.click();
+  await expect(mainpage.page).toHaveURL(/activity/);
+  await mainpage.RoadmapBtn.click();
+  await expect(mainpage.page).toHaveURL(/roadmap/);
+  await mainpage.IssuesBtn.click();
+  await expect(mainpage.page).toHaveURL(/issues/);
+  await mainpage.NewsBtn.click();
+  await expect(mainpage.page).toHaveURL(/news/);
+  await mainpage.WikiBtn.click();
+  await expect(mainpage.page).toHaveURL(/wiki/);
+  await mainpage.ForumsBtn.click();
+  await expect(mainpage.page).toHaveURL(/boards/);
+  await mainpage.RepositoryBtn.click();
+  await expect(mainpage.page).toHaveURL(/repository/);
+});
 
-test('Top menu buttons fucntionality', async ({page}) => {
-  const mainpage = new MainPage (page)
+test("Top menu buttons fucntionality", async ({ page }) => {
+  const mainpage = new MainPage(page);
 
-  await mainpage.goto()
-  await mainpage.HomeBtn.click()
-  await expect(mainpage.page).toHaveURL('https://www.redmine.org/')
-  await mainpage.HelpBtn.click()
-  await expect(mainpage.page).toHaveURL(/guide/)
-  await mainpage.ProjectsBtn.click()
-  await expect(mainpage.page).toHaveURL(/projects/)
-})
+  await mainpage.goto();
+  await mainpage.HomeBtn.click();
+  await expect(mainpage.page).toHaveURL("https://www.redmine.org/");
+  await mainpage.HelpBtn.click();
+  await expect(mainpage.page).toHaveURL(/guide/);
+  await mainpage.ProjectsBtn.click();
+  await expect(mainpage.page).toHaveURL(/projects/);
+});
